9f25f242b2f23270e3124d87c973f694e8515895
likawka <68962708+likawka@users.noreply.github.com>
2021-10-29 22:30:22 +0300

    add gameArea

--- Agent 001/GameScene.swift
+++ Agent 001/GameScene.swift
@@ -13,7 +13,23 @@ class GameScene: SKScene {
     let player = SKSpriteNode(imageNamed: "playerCat")
     let bulletSound = SKAction.playSoundFileNamed("pewPoP.mp3", waitForCompletion: true)
     
-//    var gameArea
+    var gameArea: CGRect
+    
+    override init(size: CGSize) {
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let playebleWigth = size.height / maxAspectRatio
+        let margin = (size.width - playebleWigth) / 2.0
+        
+        gameArea = CGRect(x: margin, y: 0.0, width: playebleWigth, height: size.height)
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+  
     
     // розміщення бекграунда + гравець
     override func didMove(to view: SKView) {
@@ -63,6 +79,16 @@ class GameScene: SKScene {
             let amountDragged = pointOfTouch.x - previousPointOfTouch.x
             
             player.position.x += amountDragged
+            
+            if player.position.x > gameArea.maxX{
+                player.position.x = gameArea.maxX
+                
+            }
+            if player.position.x > gameArea.minX{
+                player.position.x = gameArea.minX
+                
+            }
+                
         }
     }
     
